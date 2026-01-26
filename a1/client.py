9325d922b6adb96c43e8dff42ea9119bec1ab28f
averageandyyy <averageandyyy@gmail.com>
2026-01-26 11:37:30 -0500

    working locally

---
+++ a1/client.py
@@ -0,0 +1,110 @@
+import sys
+from socket import *
+
+
+class Client:
+    def __init__(
+        self,
+        server_address="localhost",
+        server_port=1025,
+        mode="ACTV",
+        filename="hello_world.txt",
+    ):
+        self.server_address = server_address
+        self.server_port = server_port
+        self.mode = mode
+        self.filename = filename
+        self.udp_client_socket = socket(AF_INET, SOCK_DGRAM)  # UDP
+        self.tcp_client_socket = socket(AF_INET, SOCK_STREAM)  # TCP
+
+    def send_get_request(self):
+        """
+        Send GET request to the server via UDP socket.
+        The request contains the filename and port number of TCP client socket
+        for the server to send the requested file on.
+        """
+        if self.mode == "ACTV":
+            self.tcp_client_socket.bind(("", 0))  # Bind to any available port
+            self.tcp_client_socket.listen(1)
+            tcp_port = self.tcp_client_socket.getsockname()[1]
+            get_request = f"GET {self.filename} {tcp_port}"
+            self.udp_client_socket.sendto(
+                get_request.encode(), (self.server_address, self.server_port)
+            )
+            print(f"Sent GET request: {get_request}")
+
+            # Accept 200 OK or 404 NOT FOUND response from server
+            response, _ = self.udp_client_socket.recvfrom(2048)
+            response = response.decode()
+            print(f"Received response: {response}")
+            if response == "404 NOT FOUND":
+                print(f"File '{self.filename}' not found on server.")
+                self.tcp_client_socket.close()
+                self.udp_client_socket.close()
+                return
+
+            # Receive file via TCP
+            conn, addr = (
+                self.tcp_client_socket.accept()
+            )  # conn is on the same port as tcp_client_socket
+            print(f"Connected to server at {addr} via TCP")
+            with open(f"received_{self.filename}", "wb") as file:
+                data = conn.recv(1024)
+                while data:
+                    file.write(data)
+                    data = conn.recv(1024)
+            print(f"File 'received_{self.filename}' received successfully.")
+            conn.close()
+            self.tcp_client_socket.close()
+        elif self.mode == "PASV":
+            get_request = f"GET {self.filename} PASV"
+            self.udp_client_socket.sendto(
+                get_request.encode(), (self.server_address, self.server_port)
+            )
+            print(f"Sent GET request: {get_request}")
+
+            # Receive PASV response from server
+            response, _ = self.udp_client_socket.recvfrom(2048)
+            response = response.decode()
+            print(f"Received response: {response}")
+            parts = response.split()
+            if len(parts) == 2 and parts[0] == "PASV":
+                pasv_port = int(parts[1])
+                # Create TCP socket to receive the file
+                self.tcp_client_socket.connect((self.server_address, pasv_port))
+                print(
+                    f"Connected to server at {(self.server_address, pasv_port)} via TCP"
+                )
+
+                # Receive file via TCP
+                with open(f"received_{self.filename}", "wb") as file:
+                    data = self.tcp_client_socket.recv(1024)
+                    while data:
+                        file.write(data)
+                        data = self.tcp_client_socket.recv(1024)
+                print(f"File 'received_{self.filename}' received successfully.")
+                self.tcp_client_socket.close()
+            else:
+                print("Invalid PASV response from server.")
+        else:
+            print("Invalid mode. Use 'ACTV' or 'PASV'.")
+
+
+def main():
+    if len(sys.argv) != 5:
+        print(
+            "Usage: python client.py <server_address> <server_port> <mode> <filename>"
+        )
+        sys.exit(1)
+
+    server_address = sys.argv[1]
+    server_port = int(sys.argv[2])
+    mode = sys.argv[3]
+    filename = sys.argv[4]
+
+    client = Client(server_address, server_port, mode, filename)
+    client.send_get_request()
+
+
+if __name__ == "__main__":
+    main()
---
+++ a1/server.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+import sys
+from socket import *
+
+
+class Server:
+    def __init__(self, host="localhost", port=1025, storage_directory="."):
+        self.host = host
+        self.port = port
+        self.storage_directory = storage_directory
+        self.server_socket = socket(AF_INET, SOCK_DGRAM)  # UDP
+        self.server_socket.bind((self.host, self.port))
+        print(f"SERVER_PORT={self.port}")
+
+    def receive_passive_get_request(self):
+        """
+        Receive GET request via UDP socket and creates new TCP socket to send the requested file.
+        """
+        while True:
+            print("Waiting for GET request...")
+            message, client_address = self.server_socket.recvfrom(2048)
+            print(f"Received message: {message.decode()} from {client_address}")
+
+            parts = message.decode().split()
+            if len(parts) == 3 and parts[0] == "GET":
+                filename = parts[1]
+
+                try:
+                    file = open(f"{self.storage_directory}/{filename}", "rb")
+                    if parts[2] == "PASV":
+                        tcp_server_socket = socket(AF_INET, SOCK_STREAM)
+                        tcp_server_socket.bind(("", 0))  # Bind to any available port
+                        tcp_server_socket.listen(1)
+                        pasv_port = tcp_server_socket.getsockname()[1]
+                        # Send PASV response via UDP
+                        pasv_response = f"PASV {pasv_port}"
+                        self.server_socket.sendto(
+                            pasv_response.encode(), client_address
+                        )
+                        print(
+                            f"Sent PASV response: {pasv_response} to {client_address}"
+                        )
+
+                        # Accept connection from client
+                        conn, addr = tcp_server_socket.accept()
+                        print(f"Connected to client at {addr} via TCP")
+                        # Send file via TCP
+                        data = file.read(1024)
+                        while data:
+                            conn.send(data)
+                            data = file.read(1024)
+                        print(f"File '{filename}' sent successfully.")
+                        conn.close()
+                        tcp_server_socket.close()
+                    else:  # Port Number from Client
+                        tcp_port = int(parts[2])
+                        # Create TCP socket to send the file
+                        tcp_server_socket = socket(AF_INET, SOCK_STREAM)
+                        tcp_server_socket.connect((client_address[0], tcp_port))
+                        print(
+                            f"Connected to client at {(client_address[0], tcp_port)} via TCP"
+                        )
+
+                        # Send 200 OK response via UDP
+                        self.server_socket.sendto("200 OK".encode(), client_address)
+                        print(f"Sent 200 OK to {client_address}")
+
+                        # Send file via TCP
+                        data = file.read(1024)
+                        while data:
+                            tcp_server_socket.send(data)
+                            data = file.read(1024)
+                        print(f"File '{filename}' sent successfully.")
+
+                        tcp_server_socket.close()
+                    file.close()
+                except FileNotFoundError:
+                    # Send 404 NOT FOUND response via UDP
+                    self.server_socket.sendto("404 NOT FOUND".encode(), client_address)
+                    print(
+                        f"File '{filename}' not found. Sent 404 NOT FOUND to {client_address}"
+                    )
+                    continue
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python server.py <storage_directory>")
+        sys.exit(1)
+
+    storage_directory = sys.argv[1]
+    server = Server(storage_directory=storage_directory)
+    server.receive_passive_get_request()
+
+
+if __name__ == "__main__":
+    main()
